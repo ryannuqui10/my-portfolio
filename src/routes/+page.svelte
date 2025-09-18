5523081404856bf3989ea8d85042252b2762323a
Ryan Nuqui <ryannuqui@berkeley.edu>
2025-09-18 11:47:59 -0400

    Finished Step 2.2 in Lab

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <nav>
+        <a class="current" href="/"> Home Page </a>
+        <a href="projects/"> My Projects </a>
+        <a href="contact/"> Contact Me </a>
+        <a href="https://github.com/ryannuqui10" target="_blank"> My Github Profile </a>
+    </nav>
+    <h1>Ryan Nuqui</h1>
+    <img id="profilePicture" src="images/ryan.jpg" alt="A picture of Ryan Nuqui">
+    <p>
+        Hi, I'm Ryan! I'm passionate about CS and Math Education
+        with a particular focus on making it more accessible 
+        to diverse communities. 
+    </p>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,22 @@
+<nav>
+    <a href="../"> Home Page </a>
+    <a href="../projects"> My Projects </a>
+    <a class="current" href="../contact"> Contact Me </a>
+    <a href="https://github.com/ryannuqui10" target="_blank"> My Github Profile </a>
+</nav>
+<h1> Contact Me </h1>
+<form action = "mailto:rnuqui@andrew.cmu.edu" method="POST" enctype="text/plain"> 
+    <label>
+        Email:
+        <input name="email" type="email">
+    </label>
+    <label>
+        Subject: 
+        <input name="subject">
+    </label>
+    <label>
+        Body: 
+        <textarea name="body"> </textarea>
+    </label>
+    <button> Submit </button>
+</form>
